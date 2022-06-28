ff30000611f609c34988db59b1247d35e61a4281
Thina <Thina@zipcodersmbp42.mynetworksettings.com>
2022-06-27 21:48:13 -0400

    completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
-
-    public static void main(String[] args){
-
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the number value: ");
+        int num = scan.nextInt();
+        int sum = 0;
+        for (int i = 0; i<num; i++){
+            sum  += i;
+        }
+        System.out.println("Sum of numbers : " + sum);
     }
 }
